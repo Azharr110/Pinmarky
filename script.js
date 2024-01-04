38015e4ff0830784bc3aa63c9f0706059204defc
Azharr110 <abdazhar786@gmail.com>
2024-01-04 19:38:23 +0530

    Add local storage

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
-createBtn.addEventListner("click", ()=>{
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes")
+}
+showNotes()
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML)
+}
+
+createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
@@ -10,6 +19,27 @@ createBtn.addEventListner("click", ()=>{
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 
+    // console.log("I'm clicked")
 })
 
-console.log("Hello")
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage()
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box")
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                updateStorage()
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
